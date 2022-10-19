f8f95476242008caee4b93415bb32b5652d05e91
Nercules <r0831811@student.thomasmore.be>
2022-10-19 18:08:54 +0200

    first push of opencv detection script

---
+++ code/Robot_coordinates/roboCarTracer.py
@@ -0,0 +1,46 @@
+import cv2
+
+class Color:
+    def __init__(self, lower_val, upper_val, color):
+        self.lower_val = lower_val
+        self.upper_val = upper_val
+        self.color = color
+        self.contours = None
+    
+    def create_mask(self, frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        self.mask = cv2.inRange(hsv, self.lower_val, self.upper_val)
+
+
+class Tracer:
+    def get_contours(mask):
+        contours, hierachy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours
+
+    def get_contours_center_point(contours):
+        list = []
+        if len(contours) != 0:
+            for contour in contours:
+                if cv2.contourArea(contour) > 500:
+                    # compute the center of the contour
+                    M = cv2.moments(contour)
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    list.append((cX, cY))
+        return list
+
+    def draw_center_point(frame, coordinates):
+        if len(coordinates) != 0:
+            cv2.circle(frame, coordinates, 7, (0, 0, 255), -1)
+            return frame
+
+
+class ColorObject:
+    def __init__(self, color, coordinates):
+        self.color = color
+        self.coordinates = coordinates
+
+
+class car:
+    def __init__(self, team, player_type, coordinates, angle):
+        pass
