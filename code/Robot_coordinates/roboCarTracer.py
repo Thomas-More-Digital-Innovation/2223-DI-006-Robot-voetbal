53c3b6a1252102db57958a323a7061bafccd78cf
Nercules <r0831811@student.thomasmore.be>
2022-10-21 11:19:04 +0200

    better car position filter

--- code/Robot_coordinates/roboCarTracer.py
+++ code/Robot_coordinates/roboCarTracer.py
@@ -1,16 +1,16 @@
+from math import dist
 import cv2
-from numpy import empty
-import var
+from var import TracingParams
 
 class car:
-    def __init__(self, team, player_type, coordinates, angle):
-        pass
+    def __init__(self, coordinates=None):
+        self.coordinates = coordinates
 
 class Color:
-    def __init__(self, lower_val, upper_val, color):
+    def __init__(self, lower_val, upper_val, front_color):
         self.lower_val = lower_val
         self.upper_val = upper_val
-        self.color = color
+        self.front_color = front_color
         self.contours = None
         self.color_positions = None
     
@@ -28,7 +28,7 @@ class Tracer:
         list = []
         if len(contours) != 0:
             for contour in contours:
-                if cv2.contourArea(contour) > 500:
+                if cv2.contourArea(contour) > TracingParams.MIN_AREA_COLOR.value:
                     # compute the center of the contour
                     M = cv2.moments(contour)
                     cX = int(M["m10"] / M["m00"])
@@ -36,24 +36,22 @@ class Tracer:
                     list.append((cX, cY))
         return list
 
-    def compare_distance_center_points(points_list_1, points_list_2):
-        list = []
-        for pos1 in points_list_1:
-            for pos2 in points_list_2:
+    def compare_distance_center_points(position_list_1, position_list_2):
+        list = (position_list_1[0], position_list_1[0], 1000)
+        for pos1 in position_list_1:
+            for pos2 in position_list_2:
                 dis = ((pos2[0] - pos1[0]) ** 2 + (pos2[1] - pos1[1]) ** 2) ** 0.5
-                if dis <= 50:
-                    list.append((pos1, pos2, dis))
-
+                if dis <= TracingParams.MAX_DISTANCE_BETWEEN_COLORS.value:
+                    print(dis)
+                    #if there is more than one combination, remove the combinations with the highest distance
+                    if dis < list[2]:
+                        list = (pos1, pos2, dis)
         return list
-
-        #if there is more than one combination, remove the combinations with the highest distance
-        while len(list) > 1:
-            pass
 
     def draw_car_skeleton(frame, coordinates):
         if coordinates:
-            cv2.line(frame, coordinates[0][0], coordinates[0][1], (255,255,255), 4)
-            cv2.circle(frame, coordinates[0][0], 7, (0, 0, 255), -1)
-            cv2.circle(frame, coordinates[0][1], 7, (0, 0, 255), -1)
+            cv2.line(frame, coordinates[0], coordinates[1], (255,255,255), 4)
+            cv2.circle(frame, coordinates[0], 7, (0, 0, 255), -1)
+            cv2.circle(frame, coordinates[1], 7, (0, 0, 255), -1)
         
         return frame
