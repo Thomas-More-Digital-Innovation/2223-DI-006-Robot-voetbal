08925f33894fc1fcf4db9408abf1947c74a4928c
Nercules <r0831811@student.thomasmore.be>
2022-10-21 18:40:33 +0200

    added ball tracking and mask filter trouble shooting

--- code/Robot_coordinates/main.py
+++ code/Robot_coordinates/main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from var import Team1, Team2, PlayerOffenceOne, PlayerOffenceTwo, PlayerDefence, Ball
-from roboCarTracer import Tracer, Color, car
+from roboCarTracer import Tracer, Color, Car
 
 
 def main():
@@ -13,21 +13,22 @@ def main():
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
     # set the mask filter colors
-    color_team_1 = Color(np.array(Team1.LOWER_VAL.value), np.array(Team1.UPPER_VAL.value), Team1.FRONT_COLOR.value)
-    color_team_2 = Color(np.array(Team2.LOWER_VAL.value), np.array(Team2.UPPER_VAL.value), Team2.FRONT_COLOR.value)
-
+    color_team_1 = Color(np.array(Team1.LOWER_VAL.value), np.array(Team1.UPPER_VAL.value))
+    color_team_2 = Color(np.array(Team2.LOWER_VAL.value), np.array(Team2.UPPER_VAL.value))
     all_colors_list = [
         color_team_1, 
         color_team_2, 
         ]
+    ball = Color(np.array(Ball.LOWER_VAL.value), np.array(Ball.UPPER_VAL.value))
+
 
     # Set all of the robots
-    team_1_striker_1 = car()
-    team_1_striker_2 = car()
-    team_1_goalie = car()
-    team_2_striker_1 = car()
-    team_2_striker_2 = car()
-    team_2_goalie = car()
+    team_1_striker_1 = Car()
+    team_1_striker_2 = Car()
+    team_1_goalie = Car()
+    team_2_striker_1 = Car()
+    team_2_striker_2 = Car()
+    team_2_goalie = Car()
 
     all_robots_list = [
         team_1_striker_1, 
@@ -43,13 +44,13 @@ def main():
 
         # read webcam
         ret, frame = cam.read() 
-        # all_masks = 0
+        all_masks = 0
         for color in all_colors_list:
             # apply the mask filter color on the frame
             color.create_mask(frame)
 
-            # all_masks += color.mask
-
+            all_masks += color.mask
+            
             # get and set coordinates of all found objects
             color.contours = Tracer.get_contours(color.mask)
 
@@ -57,19 +58,25 @@ def main():
             color.color_positions = Tracer.get_contours_center_point(color.contours)
 
         team_1_striker_1.coordinates = Tracer.compare_distance_center_points(color_team_1.color_positions, color_team_2.color_positions)
-        
         frame = Tracer.draw_car_skeleton(frame, team_1_striker_1.coordinates)
+
+        ball.create_mask(frame)
+        all_masks += ball.mask
+        ball.contours = Tracer.get_contours(ball.mask)
+        ball.color_positions = Tracer.get_contours_center_point(ball.contours)
+        frame = Tracer.draw_ball_skeleton(frame, ball.color_positions)
+
+        all_masks = Tracer.draw_car_skeleton(all_masks, team_1_striker_1.coordinates)
 
         # display cam
         cv2.imshow('Webcam', frame)
-        # filter = cv2.bitwise_and(frame, frame, mask=all_masks)
-        # cv2.imshow('masked', filter)
+        filter = cv2.bitwise_and(frame, frame, mask=all_masks)
+        cv2.imshow('Masked', filter)
 
         if cv2.waitKey(1) == 27: 
             break  # esc to quit
 
     cv2.destroyAllWindows()
-
 
 
 if __name__ == '__main__':
--- code/Robot_coordinates/var.py
+++ code/Robot_coordinates/var.py
@@ -1,42 +1,37 @@
 from enum import Enum
 
 class TracingParams(Enum):
-    MIN_AREA_COLOR = 500
-    MAX_DISTANCE_BETWEEN_COLORS = 100
+    MIN_AREA_COLOR = 700
+    MAX_DISTANCE_BETWEEN_COLORS = 1000
 
 class Team1(Enum):
     LOWER_VAL = [90, 100, 50]
     UPPER_VAL = [130, 255, 255]
     COLOR_STRING = 'blue'
-    FRONT_COLOR = False
 
 
 class Team2(Enum):
     LOWER_VAL = [160, 50, 50]
     UPPER_VAL = [200, 255, 255]
     COLOR_STRING = 'pink'
-    FRONT_COLOR = False
 
 
 class PlayerOffenceOne(Enum):
     LOWER_VAL = [0, 125, 0]
     UPPER_VAL = [30, 255, 255]
     COLOR_STRING = 'yellow'
-    FRONT_COLOR = True
 
 
 class PlayerOffenceTwo(Enum):
     LOWER_VAL = None
     UPPER_VAL = None
     COLOR_STRING = 'Brown'
-    FRONT_COLOR = True
 
 
 class PlayerDefence(Enum):
     LOWER_VAL = None
     UPPER_VAL = None
     COLOR_STRING = 'purple'
-    FRONT_COLOR = True
 
 
 class Ball(Enum):
